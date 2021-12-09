f32dbd68516626bb493c127e246ac5bb8b6e01bd
Dhanush <dhanushpowerk6@gmail.com>
2021-12-09 13:52:34 +0530

    Result checker in main

---
+++ app/src/main/java/com/example/criccard/TeamList.java
@@ -0,0 +1,14 @@
+package com.example.criccard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_list);
+    }
+}
